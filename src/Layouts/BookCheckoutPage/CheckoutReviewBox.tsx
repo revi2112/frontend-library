5951e21950be2a89091ff79ad763d0bad52f73a8
Revathi <revathibhojanapalli20@gmail.com>
2026-01-26 16:21:19 -0600

    ADDED AUTH PROMPT

--- src/Layouts/BookCheckoutPage/CheckoutReviewBox.tsx
+++ src/Layouts/BookCheckoutPage/CheckoutReviewBox.tsx
@@ -3,8 +3,11 @@
 
 import { Link } from "react-router-dom";
 import BookModel from "../../Models/BookModel";
+import { useAuth0 } from "@auth0/auth0-react";
 
 export const CheckoutAndReviewBox: React.FC<{book : BookModel | undefined , mobile: boolean}> = (props) => {
+
+    const {isAuthenticated, loginWithRedirect} = useAuth0();
     return(
         <div className={props.mobile ? 'card d-flex mt-5' : 'card col-3 container d-flex mb-5' }>
             <div className="card-body">
@@ -29,6 +32,15 @@ export const CheckoutAndReviewBox: React.FC<{book : BookModel | undefined , mobi
                     </div>
                 </div>
                 <Link to='/#' className="btn btn-success btn-lg">Sign in</Link>
+                {!isAuthenticated? 
+                            <button className='btn main-color btn-lg text-white' onClick={() => loginWithRedirect()}>
+                            Sign up
+                        </button>:
+                         <Link to='/messages' type='button' className='btn main-color btn-lg px-4 me-md-2 fw-bold text-white'>
+                         Library Services
+                     </Link>
+                        
+                    }
                 <hr/>
                 <p className="mt-3">
                     This number can cahnge until placing order has been complete
